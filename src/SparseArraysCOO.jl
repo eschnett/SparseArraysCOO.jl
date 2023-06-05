a626ae814dd45abfed2f3f02b8d87f49401c420e
Erik Schnetter <schnetter@gmail.com>
2023-06-05 11:16:38 -0400

    Add some efficient linear operations

--- src/SparseArraysCOO.jl
+++ src/SparseArraysCOO.jl
@@ -2,6 +2,8 @@ module SparseArraysCOO
 
 using SparseArrays
 
+################################################################################
+
 export SparseVectorCOO
 """
     SparseVectorCOO{T}
@@ -20,24 +22,70 @@ struct SparseVectorCOO{T}
     n::Int
     I::Vector{Int}
     V::Vector{T}
-    function SparseVectorCOO{T}(n::Int) where {T}
-        @assert n ≥ 0
-        return new(n, Int[], T[])
+
+    function SparseVectorCOO{T}(n::Int, I::Vector{Int}, V::Vector{T}) where {T}
+        n ≥ 0 || throw(ArgumentError("invalid Array size"))
+        I ≢ V || throw(ArgumentError("arrays must be distinct"))
+        length(I) == length(V) || throw(ArgumentError("invalid Array dimensions"))
+        all(1 ≤ i ≤ n for i in I) || throw(BoundsError())
+        return new{T}(n, I, V)
     end
 end
 
-function Base.setindex!(S::SparseVectorCOO, v, i::Integer)
-    @assert 1 ≤ i ≤ S.n
-    push!(S.I, i)
-    push!(S.V, v)
-    return S
+SparseVectorCOO(n::Int, I::Vector{Int}, V::Vector{T}) where {T} = SparseVectorCOO{T}(n, I, V)
+
+SparseVectorCOO{T}(n::Int) where {T} = SparseVectorCOO{T}(n, Int[], T[])
+
+SparseVectorCOO{T}(x::SparseVectorCOO{T}) where {T} = SparseVectorCOO{T}(x.n, copy(x.I), copy(x.V))
+SparseVectorCOO(x::SparseVectorCOO{T}) where {T} = SparseVectorCOO{T}(x)
+
+function SparseVectorCOO{T}(x::AbstractSparseVector) where {T}
+    I, V = findnz(x)
+    return SparseVectorCOO{T}(size(x, 1), I, V)
 end
+SparseVectorCOO(x::AbstractSparseVector{T}) where {T} = SparseVectorCOO{T}(x)
+
+function Base.setindex!(x::SparseVectorCOO, v, i::Integer)
+    1 ≤ i ≤ x.n || throw(BoundsError())
+    push!(x.I, i)
+    push!(x.V, v)
+    return x
+end
+
 """
-    SparseArrays.sparse(S::SparseVectorCOO)::SparseVector
+    SparseArrays.sparse(x::SparseVectorCOO)::SparseVector
 
 Convert to the standard sparse vector representation.
 """
-SparseArrays.sparse(S::SparseVectorCOO) = sparsevec(S.I, S.V, S.n)
+SparseArrays.sparse(x::SparseVectorCOO) = sparsevec(x.I, x.V, x.n)
+
+# Expensive operations are intentionally not provided. Instead, call
+# `sparse` to switch to an efficient representation, and then apply
+# these operations. Expensive operations include e.g. comparisons and
+# `getindex`.
+
+Base.eltype(::SparseVectorCOO{T}) where {T} = T
+Base.size(x::SparseVectorCOO) = (x.n,)
+
+Base.zero(x::SparseVectorCOO{T}) where {T} = SparseVectorCOO{T}(x.n)
+
+Base.map(f, x::SparseVectorCOO) = SparseVectorCOO(x.n, copy(x.I), map(f, x.V))
+
+Base.:+(x::SparseVectorCOO) = map(+, x)
+Base.:-(x::SparseVectorCOO) = map(-, x)
+
+Base.:*(a::Number, x::SparseVectorCOO) = map(b -> a * b, x)
+Base.:*(x::SparseVectorCOO, a::Number) = map(b -> b * a, x)
+Base.:\(a::Number, x::SparseVectorCOO) = map(b -> a \ b, x)
+Base.:/(x::SparseVectorCOO, a::Number) = map(b -> b / a, x)
+
+function Base.:+(x::SparseVectorCOO, y::SparseVectorCOO)
+    size(x) == size(y) || throw(DimensionMismatch())
+    return SparseVectorCOO(x.n, vcat(x.I, y.I), vcat(x.V, y.V))
+end
+Base.:-(x::SparseVectorCOO, y::SparseVectorCOO) = x + (-y)
+
+################################################################################
 
 export SparseMatrixCOO
 """
@@ -59,26 +107,74 @@ struct SparseMatrixCOO{T}
     I::Vector{Int}
     J::Vector{Int}
     V::Vector{T}
-    function SparseMatrixCOO{T}(m::Int, n::Int) where {T}
-        @assert m ≥ 0
-        @assert n ≥ 0
-        return new(m, n, Int[], Int[], T[])
+
+    function SparseMatrixCOO{T}(m::Int, n::Int, I::Vector{Int}, J::Vector{Int}, V::Vector{T}) where {T}
+        m ≥ 0 || throw(ArgumentError("invalid Array size"))
+        n ≥ 0 || throw(ArgumentError("invalid Array size"))
+        (I ≢ J && I ≢ V && J ≢ V) || throw(ArgumentError("arrays must be distinct"))
+        length(I) == length(J) == length(V) || throw(ArgumentError("invalid Array dimensions"))
+        all(1 ≤ i ≤ m for i in I) || throw(BoundsError())
+        all(1 ≤ j ≤ n for j in J) || throw(BoundsError())
+        return new{T}(m, n, I, J, V)
     end
 end
 
-function Base.setindex!(S::SparseMatrixCOO, v, i::Integer, j::Integer)
-    @assert 1 ≤ i ≤ S.m
-    @assert 1 ≤ j ≤ S.n
-    push!(S.I, i)
-    push!(S.J, j)
-    push!(S.V, v)
-    return S
+SparseMatrixCOO(m::Int, n::Int, I::Vector{Int}, J::Vector{Int}, V::Vector{T}) where {T} = SparseMatrixCOO{T}(m, n, I, J, V)
+
+SparseMatrixCOO{T}(m::Int, n::Int) where {T} = SparseMatrixCOO{T}(m, n, Int[], Int[], T[])
+
+SparseMatrixCOO{T}(A::SparseMatrixCOO{T}) where {T} = SparseMatrixCOO{T}(A.m, A.n, copy(A.I), copy(A.j), copy(A.V))
+SparseMatrixCOO(A::SparseMatrixCOO{T}) where {T} = SparseMatrixCOO{T}(A)
+
+function SparseMatrixCOO{T}(A::AbstractSparseMatrix) where {T}
+    I, J, V = findnz(A)
+    return SparseMatrixCOO{T}(size(A, 1), size(A, 2), I, J, V)
+end
+SparseMatrixCOO(x::AbstractSparseMatrix{T}) where {T} = SparseMatrixCOO{T}(x)
+
+function Base.setindex!(A::SparseMatrixCOO, v, i::Integer, j::Integer)
+    1 ≤ i ≤ A.m || throw(BoundsError())
+    1 ≤ j ≤ A.n || throw(BoundsError())
+    push!(A.I, i)
+    push!(A.J, j)
+    push!(A.V, v)
+    return A
 end
 """
-    SparseArrays.sparse(S::SparseMatrixCOO)::SparseMatrixCSC
+    SparseArrays.sparse(A::SparseMatrixCOO)::SparseMatrixCSC
 
 Convert to the standard sparse matrix representation.
 """
-SparseArrays.sparse(S::SparseMatrixCOO) = sparse(S.I, S.J, S.V, S.m, S.n)
+SparseArrays.sparse(A::SparseMatrixCOO) = sparse(A.I, A.J, A.V, A.m, A.n)
+
+# Expensive operations are intentionally not provided. Instead, call
+# `sparse` to switch to an efficient representation, and then apply
+# these operations. Expensive operations include e.g. comparisons and
+# `getindex`.
+
+Base.eltype(::SparseMatrixCOO{T}) where {T} = T
+Base.size(A::SparseMatrixCOO) = (A.m, A.n)
+
+Base.zero(A::SparseMatrixCOO{T}) where {T} = SparseMatrixCOO{T}(A.m, A.n)
+function Base.one(A::SparseMatrixCOO{T}) where {T}
+    A.m == A.n || throw(DimensionMismatch("multiplicative identity defined only for square matrices"))
+    return SparseMatrixCOO{T}(A.m, A.m, collect(1:(A.m)), collect(1:(A.m)), ones(T, A.m))
+end
+
+Base.map(f, A::SparseMatrixCOO) = SparseMatrixCOO(A.m, A.n, copy(A.I), copy(A.J), map(f, A.V))
+
+Base.:+(A::SparseMatrixCOO) = map(+, A)
+Base.:-(A::SparseMatrixCOO) = map(-, A)
+
+Base.:*(a::Number, A::SparseMatrixCOO) = map(b -> a * b, A)
+Base.:*(A::SparseMatrixCOO, a::Number) = map(b -> b * a, A)
+Base.:\(a::Number, A::SparseMatrixCOO) = map(b -> a \ b, A)
+Base.:/(A::SparseMatrixCOO, a::Number) = map(b -> b / a, A)
+
+function Base.:+(A::SparseMatrixCOO, B::SparseMatrixCOO)
+    size(A) == size(B) || throw(DimensionMismatch())
+    return SparseMatrixCOO(A.m, A.n, vcat(A.I, B.I), vcat(A.J, B.J), vcat(A.V, B.V))
+end
+Base.:-(A::SparseMatrixCOO, B::SparseMatrixCOO) = A + (-B)
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -17,7 +17,7 @@ const BigRat = Rational{BigInt}
         nk = rand(1:n)
         for k in 1:nk
             i = rand(1:n)
-            v = BigRat(rand(Int8))//rand(1:10)
+            v = BigRat(rand(Int8)) / rand(1:10)
             x[i] = v
 
             moment0 += i^0 * v
@@ -72,7 +72,7 @@ end
         for k in 1:nk
             i = rand(1:m)
             j = rand(1:n)
-            v = BigRat(rand(Int8))//rand(1:10)
+            v = BigRat(rand(Int8)) / rand(1:10)
             A[i, j] = v
 
             moment00 += i^0 * j^0 * v
@@ -124,5 +124,98 @@ end
         @test moment20′ == moment20
         @test moment21′ == moment21
         @test moment22′ == moment22
+    end
+end
+
+@testset "Sparse vector operations" begin
+    for iter in 1:100
+        n = rand(1:10)
+
+        # We need to convert to `SparseVector{BigRat}` since the element type might otherwise be `Any`
+        x₀ = SparseVector{BigRat}(BigRat.(sprand(Int8, n, rand())) ./ rand(1:10, n))
+        y₀ = SparseVector{BigRat}(BigRat.(sprand(Int8, n, rand())) ./ rand(1:10, n))
+        z₀ = SparseVector{BigRat}(BigRat.(sprand(Int8, n, rand())) ./ rand(1:10, n))
+
+        x₀::SparseVector{BigRat,Int}
+        y₀::SparseVector{BigRat,Int}
+        z₀::SparseVector{BigRat,Int}
+
+        x = SparseVectorCOO{BigRat}(x₀)
+        y = SparseVectorCOO{BigRat}(y₀)
+        z = SparseVectorCOO{BigRat}(z₀)
+        n = zero(x)
+
+        a = BigRat(rand(Int8)) / rand(1:10)
+        b = BigRat(rand(Int8)) / rand(1:10)
+
+        @test sparse(x) == x₀
+
+        @test sparse(x + n) == sparse(x)
+        @test sparse(x + y) == sparse(y + x)
+        @test sparse(x + (y + z)) == sparse((x + y) + z)
+
+        @test sparse(+x) == sparse(n + x)
+        @test sparse(-x) == sparse(n - x)
+        @test sparse(x + (-x)) == sparse(n)
+        @test sparse(x - y) == sparse(x + (-y))
+
+        @test sparse(0 * x) == sparse(n)
+
+        @test sparse(x * a) == sparse(a * x)
+
+        @test sparse(a * (x + y)) == sparse(a * x + a * y)
+        @test sparse((a + b) * x) == sparse(a * x + b * x)
+
+        if a ≠ 0
+            @test sparse(x / a) == sparse(x * inv(a))
+            @test sparse(a \ x) == sparse(inv(a) * x)
+        end
+    end
+end
+
+@testset "Sparse matrix operations" begin
+    for iter in 1:1
+        m = rand(1:10)
+        n = rand(1:10)
+
+        # We need to convert to `SparseMatrix{BigRat}` since the element type might otherwise be `Any`y
+        A₀ = SparseMatrixCSC{BigRat}(BigRat.(sprand(Int8, m, n, rand())) ./ rand(1:10, m, n))
+        B₀ = SparseMatrixCSC{BigRat}(BigRat.(sprand(Int8, m, n, rand())) ./ rand(1:10, m, n))
+        C₀ = SparseMatrixCSC{BigRat}(BigRat.(sprand(Int8, m, n, rand())) ./ rand(1:10, m, n))
+
+        A₀::SparseMatrixCSC{BigRat,Int}
+        B₀::SparseMatrixCSC{BigRat,Int}
+        C₀::SparseMatrixCSC{BigRat,Int}
+
+        A = SparseMatrixCOO{BigRat}(A₀)
+        B = SparseMatrixCOO{BigRat}(B₀)
+        C = SparseMatrixCOO{BigRat}(C₀)
+        N = zero(A)
+
+        a = BigRat(rand(Int8)) / rand(1:10)
+        b = BigRat(rand(Int8)) / rand(1:10)
+
+        @test sparse(A) == A₀
+
+        @test sparse(A + N) == sparse(A)
+        @test sparse(A + B) == sparse(B + A)
+        @test sparse(A + (B + C)) == sparse((A + B) + C)
+
+        @test sparse(+A) == sparse(N + A)
+        @test sparse(-A) == sparse(N - A)
+        @test sparse(A + (-A)) == sparse(N)
+        @test sparse(A - B) == sparse(A + (-B))
+
+        @test sparse(0 * A) == sparse(N)
+
+        @test sparse(A * a) == sparse(a * A)
+
+        @test sparse(a * (A + B)) == sparse(a * A + a * B)
+        @test sparse((a + b) * A) == sparse(a * A + b * A)
+
+        if a ≠ 0
+            @test sparse(A / a) == sparse(A * inv(a))
+            @test sparse(a \ A) == sparse(inv(a) * A)
+        end
     end
 end
